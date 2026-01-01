dfdb32fbacd3f0fa1e088cf843fa1987bdce3b91
TheMathGeek_314 <16294237+TheMathGeek314@users.noreply.github.com>
2025-12-31 23:06:17 -0600

    Make vendor items persistent

--- IC/VendorItem.cs
+++ IC/VendorItem.cs
@@ -1,10 +1,13 @@
-﻿using ItemChanger.Items;
+﻿using ItemChanger;
+using ItemChanger.Items;
+using ItemChanger.Tags;
 
 namespace VendorRando {
     public class VendorItem: VoidItem {
         public string container;
         public VendorItem(string container) {
             this.container = container;
+            AddTag<PersistentItemTag>().Persistence = Persistence.Persistent;
         }
         public override string GetPreferredContainer() => container;
     }
